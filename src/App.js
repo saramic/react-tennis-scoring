21ab6dad58ed33a2bc74cb99946f9bdfa461e584
Michael Milewski <saramic@gmail.com>
2019-12-10 22:15:58 +1100

    have a single source of truth for scores
    Co-authored-by: Kirsten Comandich <kcomandich@gmail.com>
    Co-authored-by: Samaa Kanani <samaakanani@gmail.com>
    Co-authored-by: Christine <cschorn91@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import "./App.css";
 
 function App() {
   const [score, setScore] = useState("love all");
+  const [serverScore, setServerScore] = useState(0);
+
+  const scores = ["love", 15, 30, 40];
 
   const handleClick = () => {
-    if (score === "love all") {
-      setScore("15 love");
-    } else if (score === "15 love") {
-      setScore("30 love");
+    if (serverScore === 0) {
+      setServerScore(serverScore + 1)
+      setScore(`${scores[serverScore]} ${scores[0]}`);
+    } else if (score === `${scores[1]} ${scores[0]}`) {
+      setScore(`${scores[2]} ${scores[0]}`);
     } else {
-      setScore("40 love");
+      setScore(`${scores[3]} ${scores[0]}`);
     }
   };
 
